86adf77db07d77815c7fc317f102505ca5597f3a
cnini1996@gmail.com <cnini1996@gmail.com>
2020-09-22 18:02:30 +0200

    fix class Element

--- ProgrammationObjet/SociétéListe/listeChainee.cs
+++ ProgrammationObjet/SociétéListe/listeChainee.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Societe
-{
-    class listeChainee
+namespace ListeChainee
+{   
+    class Element
     {
+        public object Objet { get; set; }
+        public Element Suivant { get; set; }
 
+        public Element(object objet)
+        {
+            Objet = objet;
+        }
+    }
+    class Liste
+    {
+        public Element Debut;
+        public int NBElement { get; }
+        public Liste(Element debut, int nbelement)
+        {
+            Debut = debut;
+            NBElement = nbelement;
+        }
+        
+        public InsererDebut()
+        {
+
+        }
     }
 }
